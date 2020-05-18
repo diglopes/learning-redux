b96077765017f92e6ec3aede22b8746b09407919
Diego Lopes <diego-chez@hotmail.com>
2020-05-18 12:25:24 -0300

    Add card styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,21 @@ function App() {
     <div className="App">
       <h1>Exercise React-redux</h1>
       <div className="line">
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
+        <Card title="Card 1" red>
+          X
+        </Card>
+        <Card title="Card 1" blue>
+          X
+        </Card>
+      </div>
+      <div className="line">
+        <Card title="Card 2" green>
+          Y
+        </Card>
+
+        <Card title="Card 2" purple>
+          Y
+        </Card>
       </div>
     </div>
   );
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 import './card.css'
 
+function getColor({red, blue, green, purple}) {
+    if(red) return "red"
+    if(blue) return "blue"
+    if(green) return "green"
+    if(purple) return "purple"
+}
+
 export default props => (
-    <div className="card">
-        Card
+    <div className={`card ${getColor(props)}`}>
+        <div className="header">
+            <span className="title">{props.title}</span>
+        </div>
+        <div className="content">
+            {props.children}
+        </div>
     </div>
 )
